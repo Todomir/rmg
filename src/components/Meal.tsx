8ea5cf5d9b33eb0a4e54608a0872a28b4fe82fa1
Abner Rodrigues <abnerluis1001@gmail.com>
2021-02-20 22:31:10 -0300

    feat: created Meal component

--- src/components/Meal.tsx
+++ src/components/Meal.tsx
@@ -1,6 +1,34 @@
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 
-import AspectRatio from './AspectRatio'
+import AspectRatio from '@components/AspectRatio'
+
+import { AnimatePresence, motion } from 'framer-motion'
+
+const foodEmojis = [
+  '🥐',
+  '🍞',
+  '🥪',
+  '🍔',
+  '🍟',
+  '🍕',
+  '🥙',
+  '🧆',
+  '🥘',
+  '🍿',
+  '🍜',
+  '🍝',
+  '🍣',
+  '🥮',
+  '🍨',
+  '🧁',
+  '🥧',
+  '🍪',
+  '🎂',
+  '🍩',
+  '🍤',
+  '🥟'
+]
 
 function ImageSkeleton() {
   return <div className="w-full h-full bg-gray-400 animate-pulse" />
@@ -37,71 +65,142 @@ export default function Meal() {
     { refetchOnWindowFocus: false }
   )
 
-  const foodEmojis = [
-    '🥐',
-    '🍞',
-    '🥪',
-    '🍔',
-    '🍟',
-    '🍕',
-    '🥙',
-    '🧆',
-    '🥘',
-    '🍿',
-    '🍜',
-    '🍝',
-    '🍣',
-    '🥮',
-    '🍨',
-    '🧁',
-    '🥧',
-    '🍪',
-    '🎂',
-    '🍩',
-    '🍤',
-    '🥟'
-  ]
+  const [open, setOpen] = useState<boolean>(false)
+
+  const container = {
+    closed: {
+      opacity: 0,
+      x: -20,
+      transition: {
+        staggerChildren: 0.035,
+        staggerDirection: -1,
+        when: 'afterChildren'
+      }
+    },
+    open: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        staggerChildren: 0.033,
+        staggerDirection: 1,
+        when: 'beforeChildren'
+      }
+    }
+  }
+
+  const item = {
+    closed: { opacity: 0, x: -10 },
+    open: { opacity: 1, x: 0 }
+  }
 
   return (
-    <div className="w-full bg-gray-50 pb-9 px-6 mt-8 rounded-md shadow-md">
-      <header className="relative -mx-6 shadow-lg">
-        <div className="w-full h-full absolute inset-0 bg-gradient-to-t from-gray-800 opacity-60 z-10" />
+    <motion.div
+      layout
+      className="w-full bg-gray-50 pb-9 px-6 mt-8 rounded-md border border-gray-200 cursor-pointer"
+      onClick={() => setOpen(!open)}
+    >
+      <motion.header layout className="relative -mx-6 shadow-lg">
+        <motion.div
+          layout
+          className="w-full h-full absolute inset-0 bg-gradient-to-t from-gray-800 opacity-60 z-10"
+        />
 
         {isLoading ? (
           <TitleSkeleton />
         ) : (
-          <div className="absolute z-20 bottom-0 flex items-center justify-between w-full">
-            <h1 className="font-extrabold text-3xl p-4 text-white">
+          <motion.div
+            layout
+            className="absolute z-20 bottom-0 flex flex-col-reverse p-4 justify-between w-full"
+          >
+            <motion.h1
+              layout
+              className="font-extrabold text-lg sm:text-3xl text-white"
+            >
               {data.name}
-            </h1>
-            {isFetching && (
-              <span className="bg-green-500 z-20 px-2 py-1 mr-4 text-xs text-white font-bold rounded-md animate-pulse">
-                Fetching new meal{' '}
-                {
-                  foodEmojis[
-                    Math.round(Math.random() * (foodEmojis.length - 1))
-                  ]
-                }
-              </span>
-            )}
-          </div>
+            </motion.h1>
+            <AnimatePresence>
+              {isFetching && (
+                <motion.span
+                  layout
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className="bg-green-500 z-20 w-36 px-2 py-1 text-xs text-white font-bold animate-bounce rounded-md shadow-md"
+                >
+                  Fetching new meal{' '}
+                  {
+                    foodEmojis[
+                      Math.round(Math.random() * (foodEmojis.length - 1))
+                    ]
+                  }
+                </motion.span>
+              )}
+            </AnimatePresence>
+          </motion.div>
         )}
         <AspectRatio ratio={16 / 9}>
           {isLoading ? (
             <ImageSkeleton />
           ) : (
-            <img
+            <motion.img
+              layout
               className="w-full h-full object-cover object-center rounded-t-md"
               src={data.image_url}
               alt={`Picture of ${data.name}`}
             />
           )}
         </AspectRatio>
-      </header>
+      </motion.header>
 
-      <p className="text-gray-500 text-sm mt-6">
-        {isLoading ? <DescriptionSkeleton /> : data.description}
-      </p>
-    </div>
+      {isLoading ? (
+        <DescriptionSkeleton />
+      ) : (
+        <motion.p layout className="text-gray-500 text-sm mt-6">
+          {data.description}
+        </motion.p>
+      )}
+
+      <AnimatePresence>
+        {open && (
+          <motion.section
+            layout
+            variants={container}
+            initial="closed"
+            animate={open ? 'open' : 'closed'}
+            exit="closed"
+            className="mt-8 flex flex-col space-y-2"
+          >
+            <motion.h3 layout variants={item} className="text-xl font-bold">
+              Ingredients
+            </motion.h3>
+
+            {data?.ingredients.map(ingredient => (
+              <motion.p
+                layout
+                variants={item}
+                className="text-sm text-gray-500"
+                key={Math.round(Math.random() * 10000)}
+              >
+                {ingredient}
+              </motion.p>
+            ))}
+
+            <motion.div layout variants={container} className="pt-5 space-y-2">
+              {data?.instructions.map((instruction, index) => (
+                <motion.div
+                  layout
+                  variants={item}
+                  className="text-sm bg-gray-100 border border-gray-300 py-2 px-3 rounded-md"
+                  key={Math.round(Math.random() * 10000)}
+                >
+                  <h4 className="text-lg font-bold">Step {index + 1}</h4>
+                  {instruction}
+                </motion.div>
+              ))}
+            </motion.div>
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </motion.div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,44 @@ import { useQueryClient } from 'react-query'
 
 import Meal from '@components/Meal'
 
+import { AnimateSharedLayout, motion } from 'framer-motion'
+
 export default function Home() {
   const queryClient = useQueryClient()
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center p-7">
-      <main className="p-8 rounded-md ring-4 ring-gray-900 bg-gray-100 max-w-2xl">
-        <h1 className="font-black text-5xl">Random Meal Generator</h1>
-        <h2 className="font-medium text-xl">
-          Get a random recipe for your delight.
-        </h2>
-
-        <Meal />
-
-        <button
-          onClick={() => queryClient.refetchQueries('meals')}
-          className="w-full py-4 mt-4 bg-blue-500 text-white text-lg font-bold rounded-md"
+    <AnimateSharedLayout>
+      <motion.div layout className="w-screen h-screen p-5">
+        <motion.main
+          layout
+          className="max-w-2xl p-8 mx-auto rounded-md ring-4 ring-gray-900 bg-gray-100"
         >
-          Get random meal
-        </button>
-      </main>
-    </div>
+          <motion.h1 layout className="font-black text-3xl sm:text-5xl">
+            Random Meal Generator
+          </motion.h1>
+          <motion.h2 layout className="font-medium text-base sm:text-xl">
+            Get a random recipe for your delight.
+          </motion.h2>
+
+          <motion.div
+            layout
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.99 }}
+          >
+            <Meal />
+          </motion.div>
+
+          <motion.button
+            layout
+            whileHover={{ scale: 1.025, backgroundColor: '#2563EB' }}
+            whileTap={{ scale: 0.99 }}
+            onClick={() => queryClient.refetchQueries('meals')}
+            className="w-full py-4 mt-4 bg-blue-500 text-white text-lg font-bold rounded-md"
+          >
+            Get random meal
+          </motion.button>
+        </motion.main>
+      </motion.div>
+    </AnimateSharedLayout>
   )
 }
